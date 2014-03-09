0b74548ed5d9631ea70021152f3cfbff89b64c89
mcrfauzi <mcr.fauzie@gmail.com>
2014-03-09 17:36:59 +0700

    memperbaiki reference dll

---
+++ sistem terdistribusi/Perkiraan Cuaca/SerializableData/SerializableData/Data.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializableData
+{
+    public enum Command
+    {
+        Login,      //Log into the server
+        Logout,     //Logout of the server
+        Message,    //Send a text message to all the chat clients
+        List,       //Get a list of users in the chat room from the server
+        Null        //No command
+    }
+
+    [Serializable]
+	public class Data
+	{
+	    //Default constructor
+	    public Data()
+	    {
+	        this.cmdCommand = Command.Null;
+	        this.strMessage = null;
+	        this.strName = null;
+	    }       	    
+	
+	    public string strName;      //Name by which the client logs into the room
+	    public string strMessage;   //Message text
+	    public Command cmdCommand;  //Command type (login, logout, send message, etcetera)
+	}
+}
