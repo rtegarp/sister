35cbd92fd3ec8fbfe61641dae79ebd6851db20d2
chankvizard <rizal.maulidan@gmail.com>
2014-03-27 11:21:31 +0700

    RMI
    Menambahkan RMI client server

--- sistem terdistribusi/Perkiraan Cuaca/SGSserverUDP/Server/SGSserverForm.Designer.cs
+++ sistem terdistribusi/Perkiraan Cuaca/SGSserverUDP/Server/SGSserverForm.Designer.cs
@@ -29,8 +29,6 @@ namespace Server
         private void InitializeComponent()
         {
             this.txtLog = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listPlayer = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // txtLog
@@ -41,33 +39,14 @@ namespace Server
             this.txtLog.Name = "txtLog";
             this.txtLog.ReadOnly = true;
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLog.Size = new System.Drawing.Size(231, 292);
+            this.txtLog.Size = new System.Drawing.Size(384, 292);
             this.txtLog.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(250, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Pemain Online :";
-            // 
-            // listPlayer
-            // 
-            this.listPlayer.FormattingEnabled = true;
-            this.listPlayer.Location = new System.Drawing.Point(253, 31);
-            this.listPlayer.Name = "listPlayer";
-            this.listPlayer.Size = new System.Drawing.Size(141, 264);
-            this.listPlayer.TabIndex = 3;
             // 
             // SGSserverForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(409, 314);
-            this.Controls.Add(this.listPlayer);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtLog);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -83,8 +62,6 @@ namespace Server
         #endregion
 
         private System.Windows.Forms.TextBox txtLog;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listPlayer;
 
     }
 }
